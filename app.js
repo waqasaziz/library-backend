036b89d791567fd5182fdd91ec7a2ddeab86f79a
Waqas Aziz <waqasazizuae@hotmail.com>
2020-04-08 17:27:06 +0100

    added bootstrap and jquery

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ let app = express();
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, '/public/')));
+app.use('/css',express.static(path.join(__dirname,"node_modules/bootstrap/dist/css")));
+app.use('/js',express.static(path.join(__dirname,"node_modules/bootstrap/dist/js")));
+app.use('/js',express.static(path.join(__dirname,"node_modules/jquery/dist")));
+app.use('/js',express.static(path.join(__dirname,"node_modules/popper.js/dist/")));
+
+
+
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
