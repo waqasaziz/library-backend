14d8dd7ec0045e4602c0a3c3a02d5a18c9479fc2
Waqas Aziz <waqasazizuae@hotmail.com>
2020-04-09 16:43:05 +0100

    Enable supoprt of Eslint

--- app.js
+++ app.js
@@ -1,25 +1,18 @@
-let express = require('express');
-let chalk = require('chalk');
-let debug = require('debug');
-let morgan = require('morgan');
-let path = require('path');
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug');
+const morgan = require('morgan');
+const path = require('path');
 
-let app = express();
+const app = express();
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, '/public/')));
-app.use('/css',express.static(path.join(__dirname,"node_modules/bootstrap/dist/css")));
-app.use('/js',express.static(path.join(__dirname,"node_modules/bootstrap/dist/js")));
-app.use('/js',express.static(path.join(__dirname,"node_modules/jquery/dist")));
-app.use('/js',express.static(path.join(__dirname,"node_modules/popper.js/dist/")));
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/popper.js/dist/')));
 
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'index.html')));
 
-
-
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
-
-app.listen(3000, function () {
-    debug(`Listening on port ${chalk.green(3000)}`);
-});
+app.listen(3000, () => debug(`Listening on port ${chalk.green(3000)}`));
